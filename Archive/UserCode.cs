07b45454d101917339ce38031179b126e50ecf5d
Victoria Mukhametova <50040694+MukhametovaVictoria@users.noreply.github.com>
2021-09-27 20:07:41 +0500

    Add files via upload

---
+++ Archive/UserCode.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace ArchiveTask
+{
+    public class Archive
+    {
+        private Operation[] operations { get; set; }
+        public Archive(string[] serializedOperations)
+        {
+            operations = serializedOperations
+                .Select(x => JsonConvert.DeserializeObject<Operation>(x))
+                .ToArray();
+        }
+
+        public Guid[] GetOperationIds(string time)
+        {
+            var date = DateTime.Parse(time, null, System.Globalization.DateTimeStyles.RoundtripKind).ToUniversalTime();
+            return operations
+                .Where(op => DateTime.Parse(op.Time, null, System.Globalization.DateTimeStyles.RoundtripKind).ToUniversalTime() == date)
+                .Select(op => new Guid(op.OperationId))
+                .ToArray();
+        }
+    }
+
+    public class Operation
+    {
+        [JsonProperty("OperationId")]
+        public string OperationId { get; set; }
+        [JsonProperty("Time")]
+        public string Time { get; set; }
+    }
+}
